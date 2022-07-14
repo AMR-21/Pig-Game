cdb2b27d822905a980d00170b22318ab4ec8e20f
amr215 <amryasser52001@gmail.com>
2022-07-14 18:04:13 +0200

    DRY

--- script.js
+++ script.js
@@ -4,21 +4,48 @@ const diceEl = document.querySelector(".dice");
 const btnRollDice = document.querySelector(".btn-roll");
 const btnHold = document.querySelector(".btn-hold");
 const btnNewGame = document.querySelector(".btn-new-game");
+const player0El = document.getElementById("player-0");
 const player1El = document.getElementById("player-1");
-const player2El = document.getElementById("player-2");
+const current0El = document.getElementById("current-score-0");
 const current1El = document.getElementById("current-score-1");
-const current2El = document.getElementById("current-score-2");
+const score0El = document.getElementById("score-0");
 const score1El = document.getElementById("score-1");
-const score2El = document.getElementById("score-2");
-let currentPlayer = 1;
-let currentScore = 0;
-let score1 = 0;
-let score2 = 0;
+let currentPlayer, currentScore, scores;
+
+function init() {
+  currentPlayer = 0;
+  currentScore = 0;
+  scores = [0, 0];
+
+  player0El.classList.remove("winner");
+  player1El.classList.remove("winner");
+
+  player0El.classList.add("active");
+  player1El.classList.remove("active");
+
+  score0El.textContent = score1El.textContent = 0;
+  current0El.textContent = current1El.textContent = 0;
+
+  btnRollDice.classList.add("hover");
+  btnHold.classList.add("hover");
+  btnHold.disabled = false;
+  btnRollDice.disabled = false;
+
+  diceEl.classList.add("hidden");
+}
+
+init();
+
+function changePlayer() {
+  document.getElementById(`current-score-${currentPlayer}`).textContent = 0;
+  currentPlayer = currentPlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("active");
+  player1El.classList.toggle("active");
+  currentScore = 0;
+}
 
 function endGame() {
-  currentPlayer === 1
-    ? player1El.classList.add("winner")
-    : player2El.classList.add("winner");
+  document.getElementById(`player-${currentPlayer}`).classList.add("winner");
 
   btnRollDice.classList.remove("hover");
   btnHold.classList.remove("hover");
@@ -26,20 +53,8 @@ function endGame() {
   btnRollDice.disabled = true;
 }
 
-function changePlayer() {
-  const currentScoreEl = document.getElementById(
-    `current-score-${currentPlayer}`
-  );
-  currentScoreEl.textContent = 0;
-  currentPlayer = currentPlayer === 1 ? 2 : 1;
-  player1El.classList.toggle("active");
-  player2El.classList.toggle("active");
-  currentScore = 0;
-}
-
 btnRollDice.addEventListener("click", function () {
   diceEl.classList.remove("hidden");
-
   const dice = Math.floor(Math.random() * 6) + 1;
   diceEl.src = `dice-${dice}.webp`;
 
@@ -53,26 +68,12 @@ btnRollDice.addEventListener("click", function () {
 });
 
 btnHold.addEventListener("click", function () {
-  currentPlayer === 1 ? (score1 += currentScore) : (score2 += currentScore);
-  score1El.textContent = score1;
-  score2El.textContent = score2;
+  scores[currentPlayer] += currentScore;
+  document.getElementById(`score-${currentPlayer}`).textContent =
+    scores[currentPlayer];
 
-  if (score1 >= 100) endGame();
-  else if (score2 >= 100) endGame();
+  if (scores[currentPlayer] >= 100) endGame();
   else changePlayer();
 });
 
-btnNewGame.addEventListener("click", function () {
-  score1El.textContent = score2El.textContent = 0;
-  current1El.textContent = current2El.textContent = 0;
-  btnRollDice.classList.add("hover");
-  btnHold.classList.add("hover");
-  btnHold.disabled = false;
-  btnRollDice.disabled = false;
-  player1El.classList.add("active");
-  player2El.classList.remove("active");
-  player1El.classList.remove("winner");
-  player2El.classList.remove("winner");
-  currentPlayer = 1;
-  currentScore = score1 = score2 = 0;
-});
+btnNewGame.addEventListener("click", init);
